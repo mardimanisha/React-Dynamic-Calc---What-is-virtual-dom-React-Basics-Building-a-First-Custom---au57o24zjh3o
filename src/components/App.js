c3b1c49af8d78f4d9371f55d2703b586e00d4816
Manisha Mardi <118474184+mardimanisha@users.noreply.github.com>
2023-01-25 19:20:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,32 @@
-import React,{useState,useEffect} from 'react'
-import '../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
 const App = () => {
+  const [output, setOutput] = useState(0);
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
+
+  useEffect(() => {
+    setOutput(Number(value1) + Number(value2));
+  }, [value1, value2]);
 
   return (
     <div id="main">
-      <input id='input1' onChange={}/>
-       +
-      <input id='input2' onChange={}/> 
-      
-      <p id='result'></p>
+      <input
+        id="input1"
+        type="number"
+        value={value1}
+        onChange={(e) => setValue1(e.target.value)}
+      />
+      +
+      <input
+        id="input2"
+        type="number"
+        value={value2}
+        onChange={(e) => setValue2(e.target.value)}
+      />
+      <p id="result">{output}</p>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
